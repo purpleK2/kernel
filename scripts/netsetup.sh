7880676c86901de58427337558512b681dde4d83
Nekodev <97458908+NotNekodev@users.noreply.github.com>
2026-02-20 23:14:49 +0100

    yay smol logging ig

--- scripts/netsetup.sh
+++ scripts/netsetup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+echo "Setting up top0 network interface"
+
+echo "Please enter sudo password for this process"
 sudo ip tuntap add dev tap0 mode tap user $(whoami)
 sudo ip link set tap0 up
 
 sudo ip addr add 192.168.100.1/24 dev tap0
+
+echo "Done setting up tap0 network interface"
