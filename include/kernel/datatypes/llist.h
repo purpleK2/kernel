4a274e80880f074af767e48e6d4cec9d61899cfa
Omar <omarino@elshorbagy.it>
2026-09-04 19:04:45 +0200

    feat(llist): update function documentation

--- include/kernel/datatypes/llist.h
+++ include/kernel/datatypes/llist.h
@@ -26,13 +26,15 @@ void ll_append(struct ll_node** root, struct ll_node* node);
  * @param s the size to allocate
  * @param alloc a function to allocate a new node from memory, can be NULL if none.
  * @returns a pointer to the allocated region, or NULL if s can't be allocated.
+ * @note If the root node gets changed internally, the new head node will be written back to *root.
  */
 void* llalloc(struct ll_node** root, size_t s, struct ll_node* (*alloc)(size_t));
 /*
  * adds a pointer back to the linked list.
  * @param root pointer to the root node
  * @param ptr the pointer to the region
- * @param s the size of the region
+ * @param s the size of the to-be reclaimed region
+ * @note If the reclaimed pointer sits at a region before the head node, *root will become the reclaimed pointer.
  */
 void llfree(struct ll_node** root, void* ptr, size_t s);
 
