acd9b164c80b69dca329cb568fabfaff3c086152
Omar <omarino@elshorbagy.it>
2026-09-20 14:34:55 +0200

    feat(macro): round up/down

--- include/kernel/macro.h
+++ include/kernel/macro.h
@@ -1,10 +1,13 @@
 #ifndef MACRO_H
 #define MACRO_H
 
-#define LIMINEREQ      __attribute__((used, section(".limine_requests")))
+#define LIMINEREQ   __attribute__((used, section(".limine_requests")))
 #define PACKED      __attribute__((packed))
 #define ALIGNED(x)  __attribute__((aligned(x)))
 
 #define UNUSED(x) ((void)x)
+
+#define ROUND_DOWN(n, a)    ((n) & ~((a) - 1))
+#define ROUND_UP(n, a)      (((n) + ((a) - 1)) & ~((a) - 1))
 
 #endif
