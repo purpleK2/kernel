950db79bf21fcb9f561c33214288d0e8a39d305d
Omar <omarino@elshorbagy.it>
2026-09-24 09:37:04 +0200

    feat(x86_64): check for hypervisor to enable E9 port

--- src/arch/x86_64/arch.c
+++ src/arch/x86_64/arch.c
@@ -1,10 +1,12 @@
 #include <arch.h>
 #include <gdt.h>
 #include <idt.h>
+#include <cpuid.h>
 
 #include <sinks.h>
 #include <kprintf.h>
 #include <debug.h>
+#include <memory.h>
 
 static const struct sink e9_sink = {
     .putc = dputc,
@@ -12,7 +14,17 @@ static const struct sink e9_sink = {
 };
 
 void arch_entry() {
-    register_sink(&e9_sink);
+    struct cpuid_ctx ctx;
+    _cpuid(0x40000000, &ctx);
+    switch (ctx.ebx) {
+        case 0x54474354:    // "TGCT", QEMU vendor little-endian
+            register_sink(&e9_sink);
+            kprintf_trace("E9 Port detected\n");
+            break;
+        default:
+            break;
+    }
+
 
     gdt_init();
     kprintf_ok("GDT setup\n");
